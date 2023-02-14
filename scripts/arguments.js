f334558154d0b7144b5ee176870168ca887ccf5c
Colin <colinchanwh@gmail.com>
2023-02-14 15:53:04 +0800

    Update

--- scripts/arguments.js
+++ scripts/arguments.js
@@ -1,11 +1,3 @@
-/*
-Arguments to create a new event by posting a new contract with
-the parameters on chain 
-
-Onece keyed in as a trial, 
-npx hardhat verify --constructor-args scripts/arguments.js  0x9c1311eefaBF95c48B20F3BaC2Bbb9e1224D5621
-where that is the deployed contract address 
-*/
 
 module.exports = [
     
